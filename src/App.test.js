c7e17ed549a0b532c59540cbb0db7f439a3a2cf8
Tim Englart <timenglartdev@gmail.com>
2021-07-19 11:12:58 +1000

    add smoke tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
 import { render, screen } from '@testing-library/react'
+import userEvents from '@testing-library/user-event'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders the app', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+
+  // Header
+  expect(() => screen.getByText(/Visual Language Editor/i)).not.toThrow()
+
+  // Footer
+  expect(() => screen.getByText(/JCU eResearch Centre/i)).not.toThrow()
+
+  // Add Row Button
+  expect(() => screen.getByText(/Add Row/i)).not.toThrow()
+})
+
+test('adds new row', () => {
+  render(<App />)
+
+  const addRowButton = screen.getByText(/Add Row/i)
+  expect(() => screen.getByRole('cell', { name: /2/i })).not.toThrow()
+  expect(() => screen.getByRole('cell', { name: /3/i })).toThrow()
+
+  // Add Row Button
+  userEvents.click(addRowButton)
+
+  // New Row Exists
+  expect(() => screen.getByRole('cell', { name: /2/i })).not.toThrow()
+  expect(() => screen.getByRole('cell', { name: /3/i })).not.toThrow()
 })
